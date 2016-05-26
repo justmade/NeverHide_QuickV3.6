a4f5c399d2abfa4e5597d056f8ac0c3b09815377
天韵 施 <77976248@qq.com>
2016-05-26 18:23:13 +0800

    添加选择关卡的界面

--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -7,33 +7,7 @@ local GameScene = import(".GameScene")
 local NeverHideApp = import(".neverhide.NeverHideApp")
 
 function MainScene:ctor()
-    -- -- add background image
-    -- display.newSprite("MainSceneBg.jpg")
-    --     :move(display.center)
-    --     :addTo(self)
-    --
-    -- -- add play button
-
-
-    -- local playButton = cc.MenuItemImage:create("PlayButton.png", "PlayButton.png")
-    --     :onClicked(function()
-    --         self:getApp():enterScene("PlayScene")
-    --     end)
-    -- cc.Menu:create(playButton)
-    --     :move(display.cx, display.cy - 200)
-    --     :addTo(self)
-
-    -- local sp = display.newSprite()
-    -- self:addChild(sp)
-    -- self:setTouchEnabled(true)
-    -- self:setTouchMode(cc.TOUCH_MODE_ALL_AT_ONCE)
-    -- self:addNodeEventListener(cc.NODE_TOUCH_EVENT,function(event) dump(event) return  end)
-
-  -- display.runScene(GameScene:new())
-  --  local n = require("app.scenes.neverhide.NeverHideApp"):new();
   print("main")
-  -- display.replaceScene()
-  -- self:addChild(NeverHideApp:new())
   local gameScene = require("app.scenes.neverhide.ChapterScene"):new()
   display.replaceScene(gameScene , "fade" , 0.5 , cc.c3b(0,0,0))
 end
